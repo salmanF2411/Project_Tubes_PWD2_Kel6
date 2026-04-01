b10ca7487acc8ebbd416ca765d556d84f9b0764c
salmanF2411 <salman241105@email.com>
2026-04-01 07:55:20 +0700

    Penambahan tampilan dropdown di header ketika user di klik + penambahan icon

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -22,6 +22,27 @@
                     <!-- fotosaya hanya sebagai contoh, nanti menyesuaikan -->
                     <img src="{{ asset('img/fotosaya.jpg') }}" alt="Profile" class="w-full h-full object-cover">
                 </button>
+
+                <!-- Menu Dropdown -->
+                <div
+                    class="absolute right-0 mt-3 w-45 bg-white text-slate-800 rounded-xl shadow-2xl py-2 z-50 border border-blue-200">
+
+                    <div class="px-4 py-3 border-b border-slate-100 bg-slate-50">
+                        <p class="font-semibold text-slate-800">Admin Jayusman</p>
+                        <p class="text-xs text-slate-500">admin@minimarket.com</p>
+                    </div>
+
+                    <a href="#"
+                        class="flex items-center gap-3 px-4 py-2 hover:bg-slate-100 transition cursor-pointer">
+                        <i class="fa-solid fa-user text-slate-600 w-4"></i>
+                        <span>Profile</span>
+                    </a>
+                    <button
+                        class="w-full text-left flex items-center gap-3 px-4 py-2 hover:bg-red-50 transition text-red-600 cursor-pointer border-t border-slate-100 mt-1">
+                        <i class="fa-solid fa-sign-out-alt w-4"></i>
+                        <span>Logout</span>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
