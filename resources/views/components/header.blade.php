9c14e298d704577dc00471cfd8fc946ce6cf800a
salmanF2411 <salman241105@email.com>
2026-04-01 07:50:08 +0700

    penambahan tampilan user di header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -8,5 +8,21 @@
                 <p class="text-xs text-slate-400 font-bold">Management System</p>
             </div>
         </div>
+
+        <div class="flex items-center gap-6">
+            <div class="hidden md:flex flex-col items-end">
+                <span class="text-sm font-medium">Pak Jayusman</span>
+                <span class="text-xs text-slate-500 font-bold">Selamat datang 👋</span>
+            </div>
+
+            <div class="relative">
+                <button
+                    class="w-10 h-10 rounded-full flex items-center justify-center 
+                           hover:shadow-lg transition cursor-pointer border-2 border-white/40 overflow-hidden">
+                    <!-- fotosaya hanya sebagai contoh, nanti menyesuaikan -->
+                    <img src="{{ asset('img/fotosaya.jpg') }}" alt="Profile" class="w-full h-full object-cover">
+                </button>
+            </div>
+        </div>
     </div>
 </header>
