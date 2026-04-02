4c94a91920ca2717f6725c84cc7e1d978d1a4d29
Dwiky-1717 <dwikyc54@gmail.com>
2026-04-02 22:12:52 +0700

    Membuat Sidebar Produk

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -6,5 +6,11 @@
                 <span>Dashboard</span>
             </a>
         </li>
+          <li>
+            <a href="#" class="flex items-center gap-3 px-4 py-3 rounded-lg font-medium transition-all hover:bg-blue-50 text-slate-700 hover:text-blue-600">
+                <i class="fa-solid fa-box w-5"></i>
+                <span>Produk</span>
+            </a>
+        </li>
     <ul>
 </aside>
