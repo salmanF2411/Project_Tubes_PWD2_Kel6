a41a241a750b7a6d19e032d89cd3e45042fef845
Dwiky-1717 <dwikyc54@gmail.com>
2026-04-02 22:11:02 +0700

    Membuat Sidebar Dashboard

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,10 @@
+<aside class="w-55 bg-white text-slate-800 min-h-screen p-4 shadow-md border-r border-slate-200">
+    <ul class="space-y-1">
+        <li>
+            <a href="#" class="flex items-center gap-3 px-4 py-3 rounded-lg font-medium transition-all hover:bg-blue-50 text-slate-700 hover:text-blue-600">
+                <i class="fa-solid fa-chart-line w-5"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+    <ul>
+</aside>
