0a636e82e84cd239f086f2cca8caf62728a805d6
salmanF2411 <salman241105@gmail.com>
2026-04-03 17:15:40 +0700

    Menambahkan Tampilan Notifikasi Di Header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -19,6 +19,16 @@
                 <i class="fa-solid fa-search text-slate-400 ml-2"></i>
             </div>
 
+            <!-- Icon Notif-->
+            <div class="relative" x-data="{ notifOpen: false }">
+                <button
+                    class="relative p-2 text-slate-600 hover:bg-slate-100 rounded-lg transition-colors cursor-pointer">
+                    <i class="fa-solid fa-bell text-xl"></i>
+                    <span class="absolute top-1 right-1 w-5 h-5 bg-red-500 text-white text-xs rounded-full flex items-center justify-center font-bold">10</span>
+                </button>
+            </div>
+
+
             <div class="hidden md:flex flex-col items-end">
                 <span class="text-sm font-medium">Pak Jayusman</span>
                 <span class="text-xs text-slate-500 font-bold">Selamat datang 👋</span>
