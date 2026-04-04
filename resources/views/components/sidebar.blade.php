a977f2898abfe3eb91093ce59a5d6d54d080e4ba
salmanF2411 <salman241105@gmail.com>
2026-04-04 15:45:40 +0700

    Merapihkan Penulisan Kode Di Bagian Sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -6,29 +6,29 @@
                 <span>Dashboard</span>
             </a>
         </li>
-          <li>
+        <li>
             <a href="#" class="flex items-center gap-3 px-4 py-3 rounded-lg font-medium transition-all hover:bg-blue-50 text-slate-700 hover:text-blue-600">
                 <i class="fa-solid fa-box w-5"></i>
                 <span>Produk</span>
             </a>
         </li>
         <li>
-         <a href="#" class="flex items-center gap-3 px-4 py-3 rounded-lg font-medium transition-all hover:bg-blue-50 text-slate-700 hover:text-blue-600">
+            <a href="#" class="flex items-center gap-3 px-4 py-3 rounded-lg font-medium transition-all hover:bg-blue-50 text-slate-700 hover:text-blue-600">
                 <i class="fa-solid fa-receipt w-5"></i>
                 <span>Transaksi</span>
             </a>
         </li>
-         <li>
+        <li>
             <a href="#" class="flex items-center gap-3 px-4 py-3 rounded-lg font-medium transition-all hover:bg-blue-50 text-slate-700 hover:text-blue-600">
                 <i class="fa-solid fa-warehouse w-5"></i>
                 <span>Stok</span>
             </a>
         </li>
         <li>
-         <a href="#" class="flex items-center gap-3 px-4 py-3 rounded-lg font-medium transition-all hover:bg-blue-50 text-slate-700 hover:text-blue-600">
+            <a href="#" class="flex items-center gap-3 px-4 py-3 rounded-lg font-medium transition-all hover:bg-blue-50 text-slate-700 hover:text-blue-600">
                 <i class="fa-solid fa-users-gear w-5"></i>
                 <span>Kelola User</span>
             </a>
         </li>
-    <ul>
+    </ul>
 </aside>
