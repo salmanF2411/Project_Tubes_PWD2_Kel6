0abd72dcd058c1c6398683f6a2457f072dbab3f4
salmanF2411 <salman241105@gmail.com>
2026-04-06 21:29:29 +0700

    Membuat Routing Agar Setiap Menu/Halaman Dapat Diakses

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.dashboard');
 });
+
+Route::get('/produk', function () {
+    return view('pages.produk');
+});
+
+Route::get('/stok', function () {
+    return view('pages.stok');
+});
+
+Route::get('/transaksi', function () {
+    return view('pages.transaksi');
+});
+
+Route::get('/laporan-stok', function () {
+    return view('pages.laporan-stok');
+});
+
+Route::get('/laporan-transaksi', function () {
+    return view('pages.laporan-transaksi');
+});
+
+Route::get('/kelola-user', function () {
+    return view('pages.kelola-user');
+});
